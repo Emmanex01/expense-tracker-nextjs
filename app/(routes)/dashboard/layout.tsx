192710fd2ade506029daa4e8235a49f7faaa97b6
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-01-31 23:46:08 +0100

    Added ChechUserBudget function

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -1,8 +1,32 @@
-import React, { ReactNode } from 'react'
+'use client';
+import React, { ReactNode, useEffect } from 'react'
 import SideBar from './_components/SideBar'
 import DashboardHeader from './_components/DashboardHeader'
+import { db } from '@/utils/dbConfig'
+import { Budgets } from '@/utils/schema'
+import { eq } from 'drizzle-orm'
+import { useUser } from '@clerk/nextjs'
+import { Router } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export default function layout({children}: {children: ReactNode}) {
+  const {user} = useUser();
+  const router = useRouter()
+
+  useEffect(() => {
+    user&&checkUserBudget()
+  },[user])
+
+  const checkUserBudget = async () => {
+    const result = await db.select()
+    .from(Budgets)
+    .where(eq(Budgets.CreatedBy, user?.primaryEmailAddress?.emailAddress?? ""))
+
+    console.log(result)
+    if(result?.length==0) {
+      router.replace("/dashboard")
+    }
+  }
   return (
     <div>
       <div className='fixed shadow-md md:w-64 hidden md:block h-screen'>
--- utils/dbConfig.tsx
+++ utils/dbConfig.tsx
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
 const sql = neon("postgresql://Emmanuel:npg_LSC3KsXAZQI1@ep-summer-frog-a8bzn1r1-pooler.eastus2.azure.neon.tech/Expense-tracker?sslmode=require")
-const db = drizzle( sql , {schema});
+export const db = drizzle( sql , {schema});
